4b078e0f524896bd1ff94d52a43823ecff95ae57
linwei <1301488613@qq.com>
2021-08-15 21:33:35 +0800

    md5加密

--- app/src/main/java/com/linwei/passwordmanager2/Constant.kt
+++ app/src/main/java/com/linwei/passwordmanager2/Constant.kt
@@ -1,16 +1,18 @@
 package com.linwei.passwordmanager2
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.widget.Toast
+import kotlinx.android.synthetic.main.psactivity_hello.*
 
 object Constant {
-    const val BASE_URL="https://ccdcapi.alipay.com"//支付宝查询银行卡所属银行
-    const val DEFAULT_KEY_NAME="test_key"
+    const val BASE_URL = "https://ccdcapi.alipay.com"//支付宝查询银行卡所属银行
+    const val DEFAULT_KEY_NAME = "test_key"
+
+    public var logStr = ""
 
 
-
-
-    fun ToastUtil(activity: Activity,msg:String,time:Int){
-        Toast.makeText(activity,msg,time).show()
+    fun ToastUtil(activity: Activity, msg: String, time: Int) {
+        Toast.makeText(activity, msg, time).show()
     }
 }
--- app/src/main/java/com/linwei/passwordmanager2/encryp/Aes.kt
+++ app/src/main/java/com/linwei/passwordmanager2/encryp/Aes.kt
@@ -1,6 +1,6 @@
 package com.linwei.passwordmanager2.encryp
-
-import android.annotation.SuppressLint
+import android.util.Log
+import java.security.Key
 import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
@@ -9,31 +9,31 @@ import javax.crypto.spec.SecretKeySpec
  *@Auther Everet
  *@Date 2021/7/4
  **/
-class AesCryp {
+class Aes(key: String) {
+
+    private val cipher: Cipher by lazy {
+        Cipher.getInstance("AES/ECB/PKCS5Padding")//"算法/模式/补码方式"
+    }
+    private val keySpec: Key by lazy {
+        SecretKeySpec(key.toByteArray(), "AES")
+    }
 
     /**
      * 加密操作
      * @param plainText 明文，需要加密的字符串
-     * @param key 密码
      */
-    @SuppressLint("NewApi", "GetInstance")
-    fun encryp(plainText: String, key: String): String {
-        val cipher = Cipher.getInstance("AES")
-        val keySpec = SecretKeySpec(key.toByteArray(), "AES")
+    fun encryp(plainText: String): String {
         cipher.init(Cipher.ENCRYPT_MODE, keySpec)
         val encrypt = cipher.doFinal(plainText.toByteArray())
+        Log.e("TAG", "encryp: ${String(encrypt)}")
         return Base64.getEncoder().encodeToString(encrypt) //这里先转换成Base64
     }
 
     /**
      * 解密操作
      * @param cipherText 密文，需要解密的字符串
-     * @param key 密码
      */
-    @SuppressLint("NewApi", "GetInstance")
-    fun decrypt(cipherText: String, key: String): String {
-        val cipher = Cipher.getInstance("AES")
-        val keySpec = SecretKeySpec(key.toByteArray(), "AES")
+    fun decrypt(cipherText: String): String {
         cipher.init(Cipher.DECRYPT_MODE, keySpec)
         val encrypt = cipher.doFinal(Base64.getDecoder().decode(cipherText))//这里将base64的字符串转换成字节数组
         return String(encrypt)
